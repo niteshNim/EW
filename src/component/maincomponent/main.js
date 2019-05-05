55c1f45912ce70ef9d7bd507761d6d01804fb50c
xeaser <sharmarag93@gmail.com>
2019-05-05 15:47:14 +0530

    Exam

--- src/component/maincomponent/main.js
+++ src/component/maincomponent/main.js
@@ -1,14 +1,14 @@
 import React from 'react' ;
 import {Switch,Route} from 'react-router-dom';
 import {Landing} from '../landingcomponent/landing';
-import   {Login} from '../logincomponent/login';
+import {Login} from '../logincomponent/login';
 
 
 export const Main=()=>
 (
     <Switch>
-<Route exact path="/" component={Landing}/>
-<Route exact path="/login" component={Login}/>
+        <Route exact path="/" component={Landing}/>
+        <Route exact path="/login" component={Login}/>
     </Switch>
 )
 
